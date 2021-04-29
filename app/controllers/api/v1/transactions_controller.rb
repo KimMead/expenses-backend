2506a1ce4069575f61a3c6621b2f5de03416d999
KimMead <kimmeadows8@gmail.com>
2021-04-28 21:49:50 -0500

    add account controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::TransactionsController < ApplicationController
+
+    before_action :set_account
+
+    def index
+        
+    end 
+
+    def create
+        
+    end 
+
+    def show
+       
+    end 
+
+    def destroy
+       
+    end 
+
+    private
+
+    def set_account
+        @account = Account.find(params[:account_id])
+    end 
+
+    def transaction_params
+        params.require(:transaction).permit(:amount, :account_id, :kind, :date, :description)
+    end 
+
 end
+

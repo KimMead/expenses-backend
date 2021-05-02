dc5abf6a3be20ebaa6ca037e611847cdb93b1012
KimMead <kimmeadows8@gmail.com>
2021-05-02 17:03:19 -0500

    set up controllers

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -3,15 +3,23 @@ class Api::V1::TransactionsController < ApplicationController
     before_action :set_account
 
     def index
-        
-    end 
-
-    def create
-        
+        @transactions = @account.transactions
+        render json: @transactions
     end 
 
     def show
-       
+        @transaction = Transaction.find(params[:id])
+        render json: @transaction
+    end 
+
+    def create
+        @transaction = @account.transactions.new(transaction_params)
+        if @account.update_balance(@transaction) != 'Balance too low.'
+            @transaction.save
+        render json: @transaction 
+        else 
+            render json: {error: 'Balance too low.'}
+        end 
     end 
 
     def destroy

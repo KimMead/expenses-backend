22de6f7bc3239c464ba9bb83302ce06b577485cd
KimMead <kimmeadows8@gmail.com>
2021-05-26 21:16:00 -0500

    install pry

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::AccountsController < ApplicationController
+    
     def index
         @accounts = Account.all 
         render json: @accounts 
     end 
 
     def create
+        
         @account = Account.new(account_params)
         if @account.save
             render json: @account 
